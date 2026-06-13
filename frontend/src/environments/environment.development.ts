f42ccd9a99e45da0b5e6a1a3ff5b83bb33065c55
Jie Shen Khor <khorjshen@gmail.com>
2026-06-13 22:03:55 +0800

    Change environment variable

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     supabaseUrl: 'https://hoswkhsznqgdtaxmrpka.supabase.co',
     supabaseKey: 'sb_publishable_Y_V_JYXs9LW6n9pqFSh6Xw_CylZIukc',
-    backendUrl: 'https://careeros-backend.vercel.app/',
+    backendUrl: 'https://careeros-backend.vercel.app',
 };
